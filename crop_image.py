36ea8a1d3afb6517d39ada92c12675cc02e82145
kozol <kozolex@gmail.com>
2020-04-06 23:18:04 +0200

    Drawing rectangle to visualize cropping proce

--- crop_image.py
+++ crop_image.py
@@ -14,28 +14,25 @@ def crop(path, input, height, width, k, page, area):
                 pass
             k +=1
 
-img = Image.open(open('./input/test.png', 'rb'))
+img = Image.open(open('input/test.png', 'rb'))
 imgWidth, imgHeight = img.size
-crop_size = 200
+crop_size = 100
+padding = 20
 if crop_size <= imgWidth and crop_size<= imgHeight:
-    print(imgWidth, imgHeight)
-    partsWidth = int(imgWidth / crop_size)
-    partsHeight = int(imgHeight / crop_size)
-    print(partsWidth, partsHeight)
+    print('Image resolition: {0},{1}'.format(imgWidth, imgHeight))
+    partsWidth = int(imgWidth / (crop_size - (2*padding)))
+    partsHeight = int(imgHeight / (crop_size - (2*padding)))
+    print('Parts on x and y: {0},{1}'.format(partsWidth, partsHeight))
     paddingWidth = imgWidth - crop_size*int(imgWidth / crop_size)
     paddingHeight = imgHeight - crop_size*int(imgHeight / crop_size)
-    print(paddingWidth, paddingHeight)
+    print('Rest size: {0},{1}'.format(paddingWidth, paddingHeight))
 
     drawing = ImageDraw.Draw(img)
-    x, y = 10, 10
-    x1, y1 = x + crop_size
-    top_left = (50, 50)
-    bottom_right = (100, 100)
-
-    outline_width = 10
-    outline_color = "black"
+    for idx in range(0,partsWidth+1):
+        for idy in range(partsHeight+1):
+            drawing.rectangle((((idx * crop_size) - (padding*idx), (idy * crop_size) - (padding*idy)), (crop_size, crop_size)), outline="red", width=1)
 else:
     print ('Crop size is too large.' )
 
-#img.show()
+img.show()
 
